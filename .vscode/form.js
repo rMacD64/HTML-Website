be2a9eefdc50ee78b4ff2b84997d8a4ec77f0e85
rMacD64 <rmacdougall03@shaw.ca>
2024-01-05 16:54:56 -0800

    Grid is one column.

--- .vscode/form.js
+++ .vscode/form.js
@@ -1,13 +1,32 @@
+/*
+  Creates the suduko grid made of input boxes.
+  @param {string} formID - the id of the form being developed
+*/
 function formMaker(formID) {
   sudukoForm = document.getElementById(formID);
   
-  for (let i = 0; i < 9; i++) {
-    let inbox = document.createElement("input");
-    inbox.id = "box" + i;
-    inbox.name = "box" + i;
-    inbox.type = "number";
-    inbox.setAttribute("max", 9);
-    inbox.setAttribute("min", 0);
-    sudukoForm.appendChild(inbox);
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      let divi = document.createElement("div");
+      divi.appendChild(inputBox(row, col));
+      sudukoForm.appendChild(divi);
+    }
   }
+}
+
+/*
+  Makes an input with the name and id "row"-"col".
+  @param {number} row - first number in name/id
+  @param {number} col - second number in name/id
+  @returns {object} input object 
+*/
+function inputBox(row, col) {
+  let inbox = document.createElement("input");
+  inbox.id = row + "-" + col;
+  inbox.name = row + "-" + col;
+  inbox.type = "number";
+  inbox.setAttribute("max", 9);
+  inbox.setAttribute("min", 1);
+
+  return inbox;
 }
