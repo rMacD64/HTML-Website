d196666e6f97483cac047b7708a349af1c1fa1f0
rMacD64 <rmacdougall03@shaw.ca>
2024-01-06 11:05:28 -0800

    Do not need the form.

--- .vscode/form.js
+++ .vscode/form.js
@@ -1,13 +1,12 @@
 /*
   Creates the suduko grid made of input boxes.
-  @param {string} formID - the id of the form being developed
   @param {string} divID - the id of the grid container
 */
-function formMaker(formID, divID) {
+function formMaker(divID) {
   for (let row = 0; row < 9; row++) {
     for (let col = 0; col < 9; col++) {
       let divi = document.createElement("div");
-      divi.appendChild(inputBox(row, col, formID));
+      divi.appendChild(inputBox(row, col));
       document.getElementById(divID).appendChild(divi);
     }
   }
@@ -17,16 +16,14 @@ function formMaker(formID, divID) {
   Makes an input with the name and id "row"-"col".
   @param {number} row - first number in name/id
   @param {number} col - second number in name/id
-  @param {object} formID - the form to be linked to
 */
-function inputBox(row, col, formID) {
+function inputBox(row, col) {
   let inbox = document.createElement("input");
   inbox.id = row + "-" + col;
   inbox.name = row + "-" + col;
   inbox.type = "number";
   inbox.setAttribute("max", 9);
   inbox.setAttribute("min", 1);
-  inbox.setAttribute("form", formID);
 
   return inbox;
 }
