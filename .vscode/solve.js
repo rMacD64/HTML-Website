be4172d895df7e88b94abe083fa5c71ed53f4173
rMacD64 <rmacdougall03@shaw.ca>
2024-01-06 21:41:11 -0800

    Organizing data in arrays for easy searching.

--- .vscode/solve.js
+++ .vscode/solve.js
@@ -1,25 +1,71 @@
+let potentials = new Array(9)
+const grid = new Array(9);
+const colList = new Array(9);
+const cubeList = new Array(9);
+let unfound = new Set();
+
 function solve() {
-
-}
-
-/*
-    Organizes input data in a usable grid array.
-*/
-function inputParser() {
-    const grid = new Array(9);
     for (let row = 0; row < 9; row++) {
-        curRow = new Array(9);
         for (let col = 0; col < 9; col++) {
-            curRow[col] = document.getElementById(row + "-" + col).value;
-        }
-        grid[row] = curRow;
-    }
+            if (grid[row][col] == null) {   // Element empty?
+                // Do further checks
+                checker();
 
-    window.alert(grid);
+            }
+        }
+    }
+}
+
+function checker() {
+
 }
 
 /*
-    Checks the inputted values are not blank
+    Organizes input data in a list of rows and a list of columns.
+*/
+function listParser() {
+    for (let x = 0; x < 9; x++) {
+        tempRow = new Array(9);
+        tempCol = new Array(9);
+
+        for (let y = 0; y < 9; y++) {
+            tempRow[y] = document.getElementById(x + "-" + y).value;
+            tempCol[y] = document.getElementById(y + "-" + x).value;
+        }
+        grid[x] = tempRow;
+        colList[x] = tempCol;
+    }
+    window.alert(colList);
+}
+
+/*
+    Updates cubeList array with the input values.
+*/
+function cubeParser() {
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            for (let z = 0; z < 3; z++) {
+                cubeList[(3 * x + y)] = document.getElementById((3 * x + z) + "-" + (3 * y + z)).value;
+            }
+        }
+    }
+}
+
+/*
+    Creates a grid.
+    @param {number} size - the size of the grid.
+    @returns {object} gridList - an array of arrays.
+*/
+function createGrid(size) {
+    gridList = new Array(size);
+    for (let x = 0; x < size; x++) {
+        gridList[x] = new Array(size);
+    }
+    return gridList;
+}
+
+/*
+    Checks the grid is not empty.
 */
 function emptyChecker(array) {
 
